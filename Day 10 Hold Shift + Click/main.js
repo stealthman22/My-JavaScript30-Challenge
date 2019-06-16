6328efd73ea6c894fd7ddcb3efc932e80cf0bae9
stealthman22 <alaratj@hotmail.com>
2019-06-16 16:08:16 +0000

    Day 10 Hold Shift + click to select multiple

---
+++ Day 10 Hold Shift + Click/main.js
@@ -0,0 +1,65 @@
+//Will always show you keycode values.
+/* 
+document.addEventListener("keydown", (e) => {
+    console.log(e.which);
+}); */
+
+
+// My first js code that actually worked somehow.
+/* const checked = document.querySelector('input:checked');
+const items = document.querySelectorAll('.item')
+
+function show(checked) {
+
+    if (checked = true) {
+        console.log('Yes');
+    } else {
+        console.log('no')
+    }
+
+
+}
+
+
+function checkBetween() {
+
+}
+
+document.addEventListener('click', show); */
+
+
+// Grabs all checkboxes
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+
+let lastChecked; // assining to the item initially checked.
+
+// Handles ALL that happens on the click event.
+function handleCheck(e) {
+    // Check if they had the shift key down.
+    //And check that they are checking it.
+
+    let inBetween = false // Evaluates when you are inbetween  two checks
+    if (e.shiftKey && this.checked) {
+        // Run the main function
+        // Loop over every single checkbox.
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+
+            // This conditional sets and  evaluates the value of checkbox.
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween; // Set the variable to the opposite of itself.
+                console.log('What tha heck is going on!!!');
+            }
+
+            // If incetween is true then, else no.
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+//Loop over all to check for click, then run function
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
