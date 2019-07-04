638c97f97b01b1059c8a7a212f97cfbd1637791c
stealthman22 <tdotonline@gmail.com>
2019-07-04 01:01:19 +0000

    Day 20 Speech to text recognition.. working now fixed on win 10

--- Day 20 Speech recognition/main.js
+++ Day 20 Speech recognition/main.js
@@ -13,18 +13,27 @@ words.appendChild(p);
 
 // add event listener
 recognition.addEventListener('result', e => {
-    console.log(e.results);
-    /* const transcript = Array.from(e.results)
-        .map(result => result[0]) */
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+
+    if (transcript.includes('console.log')) {
+        alert('\u{1F984} \u{1F984} \u{1F984} \u{1F984} \u{1F984} \u{1F984} \u{1F984} \u{1F984}');
+    }
+
+    console.log(transcript);
 });
+
+recognition.addEventListener('end', recognition.start);
 
 // begin the recognition.
 recognition.start();
-
-
-
-
-
 
 
 
@@ -47,5 +56,18 @@ recognition.start();
 
 8. Convert this result into an array.
 
-9. map the first  item that result produced 
+9. map the  array and pick the first  item that result produced 
+
+10. Map over it again and return the transcript.
+
+11. Then call join to join the returned strings.
+
+12 Add another event listener for the end event, and pass the recognition.start function so it doen't stop recognizing.
+
+13. Set the created paragraph to equal transcript.
+
+14. Stop it from replacing older paragraphs; using an if statement thats checks for isFinal, and override preassignes value of p.
+
+you can set alets or console messages with the .includes method
+this can be hooked up to api's to trigger certain functions.
 */
