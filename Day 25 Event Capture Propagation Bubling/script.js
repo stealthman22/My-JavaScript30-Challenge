76ed3b66b5339287a08dcae9f9c58931ecb5f939
stealthman22 <tdotonline@gmail.com>
2019-07-14 17:36:19 +0000

    Extra functionality for event listeners; Event propagation and bubbling

--- Day 25 Event Capture Propagation Bubling/script.js
+++ Day 25 Event Capture Propagation Bubling/script.js
@@ -30,16 +30,16 @@ button.addEventListener('click', () => {
 
 
 /* 
-The click ripple effects happens because the browser does something called a capture (it ripples down after a click) , and then does a bubble (it ripples from top to down).
+The click ripple effects happens because the browser does something called a capture (it ripples down after a click) , and then does a bubble (it ripples from down to top).
 
 At capture, the browser is only figuring out what you want, but on bubble, it triggers the click events.
 
 Adding a third argument to our function; Capture: true, this ensures that the function is not run on the bubble up, but on the capture down. False by default.
 
 Stop propagation: e.stopPropagation(); This will say stop bubbling this event up, I clicked on the one that I wanted. It won't trigger events on the parents anymore.
-You can wrap this in an of statement.
+You can wrap this in an if statement.
 
-once: listen for a click and then unbind; It will listen fro a click then unbind the event listener, causing it not to listen for future clicks.
+once: listen for a click and then unbind; It will listen for a click then unbind the event listener, causing it not to listen for future clicks.
 Very useful in store checkouts.
 
 */
